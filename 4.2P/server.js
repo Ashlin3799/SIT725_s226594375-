952cec27b0d6580dffc6069528bad69cb2146dfe
Ashlin3799 <ashlinvrajan@gmail.com>
2026-08-04 23:34:45 +1000

    4.2P

--- 4.2P/server.js
+++ 4.2P/server.js
@@ -1,9 +1,53 @@
-var express = require("express")
-var app = express()
-app.use(express.static(__dirname+'/public'))
+const { webcrypto } = require("crypto");
+global.crypto = webcrypto;
+var express = require("express");
+var mongoose = require("mongoose");   // NEW
+
+var app = express();
+
+app.use(express.static(__dirname + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-var port = process.env.port || 3000;
-app.listen(port,()=>{
-console.log("App listening to: "+port)
-})
+mongoose.connect("mongodb://127.0.0.1:27017/bookDB");
+mongoose.connection.on("connected", () => {
+    console.log("MongoDB Connected Successfully");
+});
+
+var port = process.env.PORT || 3000;
+const BookSchema = new mongoose.Schema({
+
+    title: String,
+
+    image: String,
+
+    link: String,
+
+    description: String
+
+});
+const Book = mongoose.model("Book", BookSchema);
+app.get("/api/books", async (req, res) => {
+
+    try {
+
+        const books = await Book.find({});
+
+        res.json({
+            statusCode: 200,
+            data: books,
+            message: "Success"
+        });
+
+    } catch (err) {
+
+        res.status(500).json({
+            statusCode: 500,
+            message: err.message
+        });
+
+    }
+
+});
+app.listen(port, () => {
+    console.log("App listening to: " + port);
+});
